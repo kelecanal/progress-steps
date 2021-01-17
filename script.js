1a53e07edf200027c96f5ddec1e5c8128710e646
Kareine Elecanal <kaelecanal@gmail.com>
2021-01-16 20:14:38 -0800

    added final script for toggling buttons

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let currentActive = 1;
 
 next.addEventListener("click", () => {
   currentActive++;
-
   //console.log(currentActive);
 
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
+  update();
 });
 
 previous.addEventListener("click", () => {
@@ -21,4 +21,33 @@ previous.addEventListener("click", () => {
   if (currentActive < 1) {
     currentActive = 1;
   }
+
+  update();
 });
+
+function update() {
+  //in each circle, if the index is less than the current active, then add class of 'active' to element
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  //we want to target all actives
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  //check if the current active is = 1, disable previous button
+  if (currentActive === 1) {
+    previous.disabled = true;
+    //if the current active is at the end, (total end of circles.length), disable 'next' button
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    previous.disabled = false;
+    next.disabled = false;
+  }
+}
